f1a4d92f20a19bc3429917453b4272cbeb458011
Keijiro Takahashi <keijiro@gmail.com>
2024-01-14 22:56:45 +0900

    Light controller

--- Assets/Stage/LightController.cs
+++ Assets/Stage/LightController.cs
@@ -1,26 +1,39 @@
 using UnityEngine;
 using Unity.Mathematics;
+using UnityEngine.Rendering.HighDefinition;
 
 namespace WhiteRoom {
 
 public sealed class LightController : MonoBehaviour
 {
-    Light[] _lights;
+    HDAdditionalLightData[] _lights;
 
     void Start()
-      => _lights = GetComponentsInChildren<Light>();
+      => _lights = GetComponentsInChildren<HDAdditionalLightData>();
 
+    /*
     Color GetLightColor(float3 p, float t)
     {
         var x = noise.snoise(p.xz + math.float2(0, t * 0.4f));
         var hue = math.frac(0.8f + 0.2f * x);
         return Color.HSVToRGB(hue, 0.8f, 1);
     }
+    */
+
+    Color GetLightColor(float3 p, float t)
+    {
+        var x = noise.snoise(p.xz + math.float2(0, t * 0.1f));
+        var hue = math.frac(0.5f + 0.2f * x);
+        return Color.HSVToRGB(hue, 0.4f, 1);
+    }
 
     void Update()
     {
         foreach (var l in _lights)
+        {
             l.color = GetLightColor(l.transform.localPosition, Time.time);
+            l.intensity = 600;
+        }
     }
 }
 
