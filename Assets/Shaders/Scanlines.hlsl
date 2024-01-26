3fe542f71bb33cd884432c44e5fd450678c2d46d
Keijiro Takahashi <keijiro@gmail.com>
2024-01-26 17:38:53 +0900

    effects

---
+++ Assets/Shaders/Scanlines.hlsl
@@ -0,0 +1,15 @@
+#include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise3D.hlsl"
+
+void Scroller_float(float3 WPos, out float Mask, out float Line)
+{
+    float y1 = WPos.y * 0.5 - _Time.y;
+    float y2 = WPos.y * 90;
+    Mask = frac(y1) > 0.8;
+    Line = frac(y2) > 0.8;
+
+
+    Mask = saturate(SimplexNoise(float3(0.2, _Time.y * 8, 0.5)));
+    Mask = smoothstep(0.2, 0.7, Mask);
+
+    Line *= (1 - Mask);
+}
